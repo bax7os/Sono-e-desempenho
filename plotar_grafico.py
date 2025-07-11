3994d50d10a9a9c042fd94565d59ac2ed7d8482e
bax7os <ana.moraes@ufms.br>
2025-07-11 12:43:41 -0400

    peso e inma

--- plotar_grafico.py
+++ plotar_grafico.py
@@ -76,10 +76,78 @@ def media_por_turno(csv_in):
         dados_por_turno[turno] = medias
     plotar_grafico_turno(dados_por_turno, semestres_list)
 
+def plotar_grafico_horario(dados_por_peso, semestres):
+    plt.figure(figsize=(10, 6))
+    cores = {'primeira_manha': 'blue', 'primeira_tarde': 'orange', 'primeira_noite': 'green', 
+             'segunda_manha': 'red', 'segunda_tarde': 'purple', 'segunda_noite': 'brown'}
+    for subgrupo, medias in dados_por_peso.items():
+        plt.plot(semestres, medias, marker='o', label=subgrupo, color=cores.get(subgrupo, 'gray'))
+    plt.xlabel('Semestres')
+    plt.ylabel('Média das Disciplinas')
+    plt.title('Média das Disciplinas por peso ao Longo dos Semestres')
+    plt.xticks(rotation=45)
+    plt.legend(title='Pesos')
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.tight_layout()
+    plt.show()
+
+def media_INMA_por_horario(csv_in):
+    in_csv = csv_in
+    disciplinas_semestres = defaultdict(lambda: defaultdict(list))  # {turno: {semestre: [notas]}}
+    semestres_set = set()
+    
+    with open(in_csv, 'r', newline='', encoding='utf-8') as file_in:
+        reader = csv.reader(file_in)
+        next(reader)
+        
+        for row in reader:
+            carga = int(row[17])
+            alunos = int(row[16])
+            bloco = row[14]
+            if carga <= 3 and alunos > 5 and bloco != "N/A" and bloco == "INMA":
+          
+                semestre_ano = row[6]
+                peso = float(row[18])
+                turno = row[15]
+                media_sala = float(row[19].replace(',', '.'))
+                # MANHA POSSIVEIS
+                #if peso == 1:
+                 #   subgrupo = f"primeira_{turno.lower()}"
+                #elif peso > 1 and peso <= 2:
+                 #   subgrupo = f"segunda_{turno.lower()}"
+                # TARDE POSSIVEIS
+                #if peso == 3:
+                 #   subgrupo = f"primeira_{turno.lower()}"
+                #elif peso > 3 and peso <= 4:
+                 #   subgrupo = f"segunda_{turno.lower()}"
+               
+                # NOITE POSSIVEIS
+                if peso == 5:
+                    subgrupo = f"primeira_{turno.lower()}"
+                elif peso > 5 and peso <= 6:
+                    subgrupo = f"segunda_{turno.lower()}"
+                else:
+                   # print(f"Peso {peso} não mapeado para o turno {turno}. Ignorando. carga: {carga}")
+                    continue
+                disciplinas_semestres[subgrupo][semestre_ano].append(media_sala)
+                semestres_set.add(semestre_ano)
+    semestres_list = sorted(semestres_set)
+    dados_por_peso = {}
+    for subgrupo, semestres in disciplinas_semestres.items():
+        medias = []
+        for semestre in semestres_list:
+            medias_semestre = semestres.get(semestre, [])
+            media = sum(medias_semestre) / len(medias_semestre) if medias_semestre else None
+            medias.append(media)
+        dados_por_peso[subgrupo] = medias
+        
+    plotar_grafico_horario(dados_por_peso, semestres_list)
+
 def media_por_horario(csv_in):
     in_csv = csv_in
     disciplinas_semestres = defaultdict(lambda: defaultdict(list))  # {turno: {semestre: [notas]}}
     semestres_set = set()
+ 
     with open(in_csv, 'r', newline='', encoding='utf-8') as file_in:
         reader = csv.reader(file_in)
         next(reader)
@@ -91,22 +159,43 @@ def media_por_horario(csv_in):
             if carga <= 3 and alunos > 5 and bloco != "N/A":
           
                 semestre_ano = row[6]
-                peso = row[18]
+                peso = float(row[18])
                 turno = row[15]
-                media_sala = row[19]
-                disciplinas_semestres[turno][semestre_ano].append(float(media_sala))
+                media_sala = float(row[19].replace(',', '.'))
+                # MANHA POSSIVEIS
+                if peso == 1:
+                    subgrupo = f"primeira_{turno.lower()}"
+                elif peso > 1 and peso <= 2:
+                    subgrupo = f"segunda_{turno.lower()}"
+                # TARDE POSSIVEIS
+                elif peso == 3:
+                    subgrupo = f"primeira_{turno.lower()}"
+                elif peso > 3 and peso <= 4:
+                    subgrupo = f"segunda_{turno.lower()}"
+               
+                # NOITE POSSIVEIS
+                elif peso == 5:
+                    subgrupo = f"primeira_{turno.lower()}"
+                elif peso > 5 and peso <= 6:
+                    subgrupo = f"segunda_{turno.lower()}"
+                else:
+                   # print(f"Peso {peso} não mapeado para o turno {turno}. Ignorando. carga: {carga}")
+                    continue
+                
+                disciplinas_semestres[subgrupo][semestre_ano].append(media_sala)
                 semestres_set.add(semestre_ano)
+   
     semestres_list = sorted(semestres_set)
     dados_por_peso = {}
-    for turno, semestres in disciplinas_semestres.items():
+    for subgrupo, semestres in disciplinas_semestres.items():
         medias = []
         for semestre in semestres_list:
             medias_semestre = semestres.get(semestre, [])
-            media = sum(medias_semestre) / len(medias_semestre) if medias_semestre else 0
+            media = sum(medias_semestre) / len(medias_semestre) if medias_semestre else None
             medias.append(media)
-        dados_por_peso[turno] = medias
+        dados_por_peso[subgrupo] = medias
         
-    plotar_grafico_bloco(dados_por_peso, semestres_list)
+    plotar_grafico_horario(dados_por_peso, semestres_list)
 
 def media_por_bloco(csv_in):
     in_csv = csv_in
@@ -142,11 +231,32 @@ def media_por_bloco(csv_in):
         dados_por_bloco[bloco] = medias
     plotar_grafico_bloco(dados_por_bloco, semestres_list)
 
+def visualizar_pesos_por_turno(csv_in):
+    in_csv = csv_in
+    pesos_por_turno = defaultdict(set)
+    with open(in_csv, 'r', newline='', encoding='utf-8') as file_in:
+        reader = csv.reader(file_in)
+        next(reader)
+        for row in reader:
+            carga = int(row[17])
+            alunos = int(row[16])
+            bloco = row[14]
+            if carga <= 3 and alunos > 5 and bloco != "N/A":
+                turno = row[15].upper()
+                try:
+                    peso = float(row[18])
+                except ValueError:
+                    continue
+                pesos_por_turno[turno].add(peso)
+    for turno, pesos in pesos_por_turno.items():
+        print(f"Turno {turno} tem esses pesos possíveis: {sorted(pesos)}")
 if __name__ == "__main__":
     in_csv = 'resultados/materias_regulares_enriquecido.csv'
-    #media_por_turno(in_csv)
+    #media_por_turno(in_csv
     #media_por_horario(in_csv)
-    media_por_bloco(in_csv)
+    #media_por_bloco(in_csv)
+    media_INMA_por_horario(in_csv)
+    #visualizar_pesos_por_turno(in_csv)
     print("Gráfico gerado com sucesso!")
 
           
